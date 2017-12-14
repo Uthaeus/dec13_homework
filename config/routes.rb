d73737a841cd7549219312d6933d01f065e68c9f
Uthaeus <Uth@github.com>
2017-12-13 20:34:11 -0700

    Created nav bar and implemented bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/faq'
-
-  get 'pages/contact'
-
-  get 'pages/discussion'
+  
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'faq', to: 'pages#faq'
+  get 'discussion', to: 'pages#discussion'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
