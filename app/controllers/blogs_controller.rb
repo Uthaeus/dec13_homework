549b0c9a10230beff93fcb4ac8c0bfdc66ac99be
Uthaeus <romanlavery@gmail.com>
2018-07-10 10:56:19 -0600

    Created edit, show, new, and form pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,11 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
     @blogs = Blog.all
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -25,8 +26,36 @@ class BlogsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @blog.destroy
+
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed' }
+      format.json { head :no_content }
+    end
+  end
+
 
   private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 
   def blog_params
     params.require(:blog).permit(:title, :author, :date, :body)
